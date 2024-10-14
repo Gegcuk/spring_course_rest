257d63a30e10a186810c2cf977982ba55c42cd7b
alekseiLazunin <gegcuk@gmail.com>
2024-10-14 20:40:21 +0100

    feat: Add exception handling for Employee REST API
    - Created `EmployeeIncorrectData` class to structure error responses.
    - Added `NoSuchEmployeeException` for handling cases where an employee is not found.
    - Enhanced `MyRESTController`:
      - Added `/employees/{id}` endpoint to fetch an employee by ID with error handling.
      - Implemented `@ExceptionHandler` methods to handle `NoSuchEmployeeException` (404) and generic exceptions (400).
    - Improved error handling to return meaningful HTTP status codes and messages in JSON format.

--- src/main/java/uk/gegc/spring/rest/controller/MyRESTController.java
+++ src/main/java/uk/gegc/spring/rest/controller/MyRESTController.java
@@ -1,10 +1,12 @@
 package uk.gegc.spring.rest.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 import uk.gegc.spring.rest.entity.Employee;
+import uk.gegc.spring.rest.exceptionHandling.EmployeeIncorrectData;
+import uk.gegc.spring.rest.exceptionHandling.NoSuchEmployeeException;
 import uk.gegc.spring.rest.service.EmployeeService;
 
 import java.util.List;
@@ -18,6 +20,33 @@ public class MyRESTController {
     @GetMapping("/employees")
     public List<Employee> showAllEmployees() {
         return employeeService.getAllEmployees();
+    }
+
+    @GetMapping("/employees/{id}")
+    public Employee getEmployee(@PathVariable int id){
+        Employee employee = employeeService.getEmployee(id);
+
+        if(employee == null) {
+            throw new NoSuchEmployeeException("There is no employee with id = " + id + " in database");
+        }
+
+    return employee;
+    }
+
+    @ExceptionHandler
+    public ResponseEntity<EmployeeIncorrectData> handleException(NoSuchEmployeeException exception){
+        EmployeeIncorrectData data = new EmployeeIncorrectData();
+        data.setInfo(exception.getMessage());
+        return new ResponseEntity<>(data, HttpStatus.NOT_FOUND);
+    }
+
+    @ExceptionHandler
+    public ResponseEntity<EmployeeIncorrectData> handleException(Exception exception){
+        EmployeeIncorrectData data = new EmployeeIncorrectData();
+        data.setInfo(exception.getMessage());
+
+
+        return new ResponseEntity<>(data, HttpStatus.BAD_REQUEST);
     }
 
 }
---
+++ src/main/java/uk/gegc/spring/rest/exceptionHandling/NoSuchEmployeeException.java
@@ -0,0 +1,7 @@
+package uk.gegc.spring.rest.exceptionHandling;
+
+public class NoSuchEmployeeException extends RuntimeException{
+    public NoSuchEmployeeException(String message){
+        super(message);
+    }
+}
